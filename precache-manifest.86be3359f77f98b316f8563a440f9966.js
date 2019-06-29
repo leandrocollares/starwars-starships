70a29d0a7236ceb81515576b8be9ff72bace948c
Leandro Collares <leco@uvic.ca>
2019-06-29 15:57:23 -0300

    Updates

---
+++ precache-manifest.86be3359f77f98b316f8563a440f9966.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "caec8f0775e9fc40cdef",
+    "url": "/starwars-starships/static/js/runtime~main.bfe8377b.js"
+  },
+  {
+    "revision": "8ed881be96dc2026cc62",
+    "url": "/starwars-starships/static/js/main.077cf50f.chunk.js"
+  },
+  {
+    "revision": "a073bea331d423b2af75",
+    "url": "/starwars-starships/static/js/2.28571e12.chunk.js"
+  },
+  {
+    "revision": "8ed881be96dc2026cc62",
+    "url": "/starwars-starships/static/css/main.9ad8a97c.chunk.css"
+  },
+  {
+    "revision": "cace8d95c74af514b88cf4d29df8c9c7",
+    "url": "/starwars-starships/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/starwars-starships/precache-manifest.86be3359f77f98b316f8563a440f9966.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/starwars-starships/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
